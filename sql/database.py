5abdf76ea5b6901f119281c3a8f6d1c7621d7e16
M1 <vilarmatheus123@gmail.com>
2024-10-16 20:54:10 -0300

    Incluir dados da string de conexão do gitignore

--- sql/database.py
+++ sql/database.py
@@ -2,18 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from connection_string import con_str
 
-server = "techchallenge.database.windows.net"
-database = "SQL_FIAP"
-user = "azuredbbase"
-password = "TechChallenge@321"
-
-#driver = "SQL Server"
-driver = "ODBC Driver 18 for SQL Server"
-        
-connection_string = f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={user};PWD={password}'
-connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
-engine = create_engine(connection_url, use_setinputsizes = False, echo = False, fast_executemany = True)
+connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": con_str})
+engine = create_engine(con_str, use_setinputsizes = False, echo = False, fast_executemany = True)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
