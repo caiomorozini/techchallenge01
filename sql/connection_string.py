68edbf85dfea3daa66e356faa72afde7ce132086
M1 <vilarmatheus123@gmail.com>
2024-10-16 20:57:27 -0300

    Remover connection_string.py do índice de rastreamento

--- sql/connection_string.py
+++
@@ -1,7 +0,0 @@
-server = "techchallenge.database.windows.net"
-database = "SQL_FIAP"
-user = "azuredbbase"
-password = "TechChallenge@321"
-driver = "ODBC Driver 18 for SQL Server"
-        
-con_str = f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={user};PWD={password}'
